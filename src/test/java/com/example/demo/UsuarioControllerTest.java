fd35e63f643a5cf613044c93fc1544057ed3ef14
Pau Albarracín Granell <tacauzumaki837@gmail.com>
2026-01-13 12:45:47 +0100

    dc

---
+++ src/test/java/com/example/demo/UsuarioControllerTest.java
@@ -0,0 +1,22 @@
+package com.example.demo;
+
+import com.example.demo.controller.UsuarioController;
+import com.example.demo.repository.UsuarioRepository;
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.beans.factory.annotation.Autowired;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+@SpringBootTest
+public class UsuarioControllerTest {
+
+    @Autowired
+    private UsuarioController usuarioController;
+
+    @Test
+    void contextLoads() {
+        // Verifica que el controlador se haya cargado correctamente en el contexto
+        assertThat(usuarioController).isNotNull();
+    }
+}
